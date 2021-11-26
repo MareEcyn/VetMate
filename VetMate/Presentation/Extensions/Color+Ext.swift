e0824eb6c0bc1d9f8cc6b4554aba125d1865d44c
ecyn <ecynmare@gmail.com>
2021-11-26 12:11:00 +0700

    add colors

--- VetMate/Presentation/Extensions/Color+Ext.swift
+++ VetMate/Presentation/Extensions/Color+Ext.swift
@@ -5,4 +5,7 @@ extension Color {
     public static let group = Color.init("Group")
     public static let interactiveBlue = Color.init("InteractiveBlue")
     public static let calculationResult = Color.init("Green")
+    public static let fieldName = Color.init("FieldName")
+    public static let favorite = Color.init("Favorite")
+    public static let calculatorResult = Color.init("CalculatorResult")
 }
